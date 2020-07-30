1be7011e8bb357e8d60e35be0d72bd8d31a305ce
Kevin <bourkekev@gmail.com>
2020-07-30 21:07:32 +0100

    Add connections to MongoDB, set env variables for mongo

--- app.py
+++ app.py
@@ -3,12 +3,21 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+from os import path
+if path.exists("env.py"):
+   import env
+
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'your_recipe_hub'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-   return render_template("index.html")
+@app.route('/get_recipes')
+def get_recipes():
+   return render_template("index.html", recipes=mongo.db.recipes.find())
  
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
