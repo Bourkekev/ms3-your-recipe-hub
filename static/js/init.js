709fe2633286f29906712abce08a17c088691d96
Kevin <bourkekev@gmail.com>
2020-08-04 08:08:49 +0100

    Create basic add recipes form fields with icons

--- static/js/init.js
+++ static/js/init.js
@@ -3,6 +3,7 @@
   
       $('.sidenav').sidenav();
       $('.parallax').parallax();
+      $('select').formSelect();
   
     }); // end of document ready
   })(jQuery); // end of jQuery name space
