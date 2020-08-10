1386095996a9def0139d977c72969d5b2115afa9
Kevin <bourkekev@gmail.com>
2020-08-10 21:19:45 +0100

    Added route for category passed via link parameter

--- app.py
+++ app.py
@@ -49,14 +49,20 @@ def course_list(course_name):
    return render_template("all-recipes.html", recipes=recipes)
 
 
-@app.route('/category/<category_name>', methods=["GET", "POST"])
-def category_list(category_name):
+@app.route('/category', methods=["GET", "POST"])
+def category_list():
    if request.method == "POST":
       category = request.form.get("category_name")
       #return(str(category))
       recipes = list(mongo.db.recipes.find({"category_name": category}))
       return render_template("all-recipes.html", recipes=recipes)
 
+   recipes = list(mongo.db.recipes.find({"category_name": category_name}))
+   return render_template("all-recipes.html", recipes=recipes)
+
+
+@app.route('/category/<category_name>')
+def category_list_url(category_name):
    recipes = list(mongo.db.recipes.find({"category_name": category_name}))
    return render_template("all-recipes.html", recipes=recipes)
 
