6033702482ddb882926cbec2dc019ad50c1dba78
Kevin <bourkekev@gmail.com>
2020-08-12 19:06:01 +0100

    Replace logo with svg version

--- static/js/init.js
+++ static/js/init.js
@@ -9,10 +9,8 @@
         $(window).scroll(function() {
             // Track scroll amount
             let yPos = ( $(window).scrollTop() );
-            console.log(yPos)
             // Get header height
             let headerHeight = ( $('header').outerHeight() );
-            console.log(headerHeight)
 
             //When scroll is greater than header
             if(yPos > headerHeight ) { 
